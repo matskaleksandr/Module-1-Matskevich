03ef525c9663a2a55fbb7d6b76ba434332a085f8
AleksandrMatskevich <kokoseke06@gmail.com>
2023-09-26 08:29:28 +0300

    Добавьте файлы проекта.

---
+++ Modul 1 Мацкевич/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_1_Мацкевич
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
