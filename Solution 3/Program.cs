192586c6fd29fb985c5cd1175895dd7e5a1b0d45
AleksandrMatskevich <kokoseke06@gmail.com>
2023-09-26 09:36:48 +0300

    Обновлён Solution 3

--- Solution 3/Program.cs
+++ Solution 3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Solution_3
 {
@@ -10,6 +11,30 @@ namespace Solution_3
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string surname = "";
+                string name = "";
+                string pattern = @"^[А-ЯЁ][а-яё]*$";
+                Console.WriteLine("Введите фамилию");
+                do
+                {
+                    surname = Console.ReadLine();
+                }
+                while (!Regex.IsMatch(surname, pattern));
+                Console.WriteLine("Введите имя");
+                do
+                {
+                    name = Console.ReadLine();
+                }
+                while (!Regex.IsMatch(name, pattern));
+                Console.WriteLine();
+                Console.WriteLine($"{surname}, {name}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
