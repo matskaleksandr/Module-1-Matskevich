f1d1f259629e639065e04ec35798070152398333
AleksandrMatskevich <kokoseke06@gmail.com>
2023-09-26 10:09:49 +0300

    Обновлён Solution 2

--- Solution 2/Program.cs
+++ Solution 2/Program.cs
@@ -10,8 +10,31 @@ namespace Solution_2
     {
         static void Main(string[] args)
         {
-            
-            
+            double radius;
+            string radiusInput;
+            double area = 0;
+            try
+            {
+                do
+                {
+                    Console.Write("Введите радиус круга: ");
+                    radiusInput = Console.ReadLine();
+                }
+                while (!double.TryParse(radiusInput, out radius));
+                if (radius >= 0)
+                {
+                    area = Math.PI * radius * radius;
+                    Console.WriteLine($"Площадь круга с радиусом {radius} равна: {area}");
+                }
+                else
+                {
+                    Console.WriteLine("Радиус не может быть отрицательным");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
