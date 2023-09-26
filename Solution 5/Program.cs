21063e1735e89b96bb10169a67157ff7f83501e6
AleksandrMatskevich <kokoseke06@gmail.com>
2023-09-26 08:39:09 +0300

    Обновлён Solution 5

--- Solution 5/Program.cs
+++ Solution 5/Program.cs
@@ -10,6 +10,27 @@ namespace Solution_5
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int iAge = 0;
+                string sAge = "";
+                int iValue;
+                do
+                {
+                    Console.WriteLine("Сколько вам лет");
+                    sAge = Console.ReadLine();
+                }
+                while (int.TryParse(sAge, out iValue) != true);
+                iAge = Convert.ToInt32(sAge);
+                if (iAge > 18)
+                    Console.WriteLine("Вы можете получить водительские права");
+                else
+                    Console.WriteLine("Вы не можете получить водительские права");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
