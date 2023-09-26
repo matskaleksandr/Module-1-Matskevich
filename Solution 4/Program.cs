c190dbfeb132962be828d4db6b67a32bab2d2038
AleksandrMatskevich <kokoseke06@gmail.com>
2023-09-26 08:44:40 +0300

    Обновлён Solution 4

--- Solution 4/Program.cs
+++ Solution 4/Program.cs
@@ -10,6 +10,34 @@ namespace Solution_4
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int maxValue = 0;
+                int minValue = 0;
+                Random random = new Random();
+                int[] randomArray = new int[20];
+                Console.WriteLine("Массив:");
+                for (int i = 0; i < randomArray.Length; i++)
+                {
+                    randomArray[i] = random.Next(-100, 100);
+                    Console.WriteLine(randomArray[i]);
+                }
+                for (int i = 1; i < randomArray.Length; i++)
+                {
+                    if (randomArray[i] < minValue)
+                        minValue = randomArray[i];
+
+                    if (randomArray[i] > maxValue)
+                        maxValue = randomArray[i];
+                }
+                Console.WriteLine($"Максимальный элемент массива:{maxValue}");
+                Console.WriteLine($"Минимальный элемент массива:{minValue}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
+
